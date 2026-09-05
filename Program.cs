ce4c464344d89328856cccaff72049ccc47967eb
Majaahh <maja.blaszczak@proton.me>
2026-09-06 01:02:46 +0200

    hextoascii: Store and write output
    Change-Id: Id4c339afcbcbf49d77f9f78c26c2b271fa326bbf
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- Program.cs
+++ Program.cs
@@ -4,14 +4,15 @@ public class Program {
     static void Main(string[] args) {
         foreach (var arg in args) {
             string hex = arg;
+            string output = "";
 
             for (int i = 0; i < hex.Length; i += 2) {
                 string chunk = hex.Substring(i, 2);
                 int dec = Convert.ToInt32(chunk, 16);
 
-                Console.Write((char)dec);
+                output += ((char)dec);
             }
-            Console.WriteLine();
+            Console.WriteLine(output);
         }
     }
 }
