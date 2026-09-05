6fc86fd94001b3763c3236a80f49b92c3eeda865
Majaahh <maja.blaszczak@proton.me>
2026-09-06 00:57:00 +0200

    hextoascii: Initial project
    Change-Id: Iff98a167b54866607f82e06603510339bcdfa802
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program {
+    static void Main(string[] args) {
+        foreach (var arg in args) {
+            string hex = arg;
+
+            for (int i = 0; i < hex.Length; i+= 2) {
+                string chunk = hex.Substring(i, 2);
+                int dec = Convert.ToInt32(chunk, 16);
+
+                Console.Write((char)dec);
+            }
+            Console.WriteLine();
+        }
+    }
+}
